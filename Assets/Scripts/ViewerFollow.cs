15347b9713bd1a245339bfaceccba9d2e3a825c2
ShahabasShakeer <39695438+ShahabasShakeer@users.noreply.github.com>
2020-02-03 13:14:20 +0530

    Added Files

---
+++ Assets/Scripts/ViewerFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewerFollow : MonoBehaviour
+{
+    public Transform target;
+    public float posX;
+    public float posZ;
+
+    void Update() {
+        posX = target.position.x - transform.position.x;
+        posZ = target.position.z - transform.position.z;
+
+        //if (posX > 100 || posZ > 100) {
+            transform.position = new Vector3(target.position.x, 1.3f, target.position.z);
+        //}
+
+        //transform.position = new Vector3(target.position.x, 1.3f, target.position.z);
+    }
+}
